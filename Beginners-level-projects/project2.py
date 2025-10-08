02deda4d0d0d90f4eb71ff3243bb255ccb37fbd9
Srijan0113 <141817720+Srijan0113@users.noreply.github.com>
2025-10-08 10:48:18 +0530

    Add files via upload

---
+++ Beginners-level-projects/project2.py
@@ -0,0 +1,40 @@
+#weather app
+import requests
+import json
+import win32com.client as wc
+
+city=input("Enter the name of the city  : \n ")
+url=f"https://api.weatherapi.com/v1/current.json?key=3a4b548dcb2c429e9b463544250609&q={city}"
+
+r=requests.get(url)
+#print(r.text)
+#print(type(r.text)) #string type 
+wdic=json.loads(r.text) #convert to python dictionary from json string
+
+speaker = wc.Dispatch("SAPI.SpVoice")
+for key,value in wdic.items():
+    
+    text=f"{key} is {value}"
+    print(text)
+    #speaker.Speak(text)
+
+keys_location=["name","region","country","lat","lon","localtime"]
+keys_current=['last_updated','temp_c','feelslike_c','temp_f','humidity','condition','wind_dir','wind_kph']
+
+print(f"---------Weather report of {city} ----------")
+for key in keys_location:
+    value=wdic["location"][key]
+    print(f"{key.capitalize()}:{value}")
+    speaker.speak(f"{key} is {value}")
+
+
+for key in keys_current:
+    value=wdic["current"][key]
+    #condition itself is a dict -> take only "text"
+    if isinstance(value,dict):
+         value=value["text"]
+         
+    print(f"{key.capitalize()}:{value}")
+    speaker.speak(f"{key} is {value}")
+
+
